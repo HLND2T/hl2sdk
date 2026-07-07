6bb1256889140544490124b93311bfb2b9bd9260
hzqst <113660872@qq.com>
2026-07-07 18:15:34 +0800

    Fix INetworkGameServer.

--- public/iserver.h
+++ public/iserver.h
@@ -74,8 +74,6 @@ public:
 	// returns current client limit
 	virtual int		GetMaxClients( void ) const = 0;
 
-	virtual float   unk001() = 0;
-
 	virtual void	ServerAdvanceTick( const EventServerAdvanceTick_t & ) = 0;
 	virtual void	ServerPollNetworking( const EventServerPollNetworking_t & ) = 0;
 	virtual void	ServerProcessNetworking( const EventServerProcessNetworking_t & ) = 0;
